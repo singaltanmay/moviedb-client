87c31d79e7678d40e7e1fd1ceedb1b97fed70d5a
sdghj <sdfbdbjhu@serngiknse.com>
2020-04-01 16:53:35 +0530

    Store user input from search box in a state variable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import Search from "./components/Search";
 
 function App() {
+
+  const [state, setState] = useState({
+    s : "",
+    results: [],
+    selected:{}
+  })
+
+  const apiKey = "8e13383"
+  
+  const handleInput = (e) => {
+    let s = e.target.value
+
+    setState(prevState => {
+      return {...prevState, s : s}
+    })
+
+    console.log(state.s)
+
+  }
+
   return (
     <div className="App">
       <header>
         <h1>Movie Database</h1>
       </header>
       <main>
-        <Search />
+        <Search  handleInput={handleInput}/>
       </main>
     </div>
   );
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-const Search = () => {
+const Search = ({handleInput}) => {
   return (
     <section className="searchbox-wrap">
       <input
         type="text"
         placeholder="Search for a movie"
         className="searchbox"
+        onChange={handleInput}
       ></input>
     </section>
   );
