89bdecfc7a63138bc39241e2b7fc64cfa320e017
singaltanmay <tanmaysingal@icloud.com>
2020-07-05 18:10:54 +0530

    Updates

---
+++ precache-manifest.97c798ab6b60ff54146fe54ef4ee64f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772b1e5b89a2be3f3bd48d2cb755378f",
+    "url": "/moviedb-client/index.html"
+  },
+  {
+    "revision": "666a6b7d15b2e14aae92",
+    "url": "/moviedb-client/static/css/main.43bb9306.chunk.css"
+  },
+  {
+    "revision": "c6efefffbcec5c60fa5b",
+    "url": "/moviedb-client/static/js/2.c59b68e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviedb-client/static/js/2.c59b68e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "666a6b7d15b2e14aae92",
+    "url": "/moviedb-client/static/js/main.6005c9a1.chunk.js"
+  },
+  {
+    "revision": "28408437ffe62bd50a64",
+    "url": "/moviedb-client/static/js/runtime-main.114e990c.js"
+  }
+]);
